d3097defdf1fc13d499c7d16d34fe926460592b6
Michael Robinson <laxdog@gmail.com>
2019-04-15 16:28:39 +0000

    Initial packaging commit

---
+++ Untitled Folder/quotes.py
@@ -0,0 +1,8 @@
+single_quote = "Easy things make life harder, hard things make life easier"
+multi_quote = ["Collect underpants", "?", "Profit"]
+
+def foo():
+    print("bar")
+    
+class Bar():
+    pass
