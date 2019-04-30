c142a2d796761ab9778abdde515f87527ec747a7
Matt Mulhern <mattmulhern01@gmail.com>
2019-04-30 14:48:43 +0100

    fixing url
    Signed-off-by: Matt Mulhern <mattmulhern01@gmail.com>

--- 05-packaging-part-2/hello-world/setup.py
+++ 05-packaging-part-2/hello-world/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='hello_world',
       version='0.0.1',
       description='Dummy package used for deployment testing.',
-      url='git@github.com:BelfastTechTraining/python.git',
+      url='https://github.com/BelfastTechTraining/python.git',
       author='Matt Mulhern',
       author_email='mattmulhern01@gmail.com',
       packages=['hello_world'],
